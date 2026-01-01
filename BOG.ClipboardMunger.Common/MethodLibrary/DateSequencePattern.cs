238f16ea2445b210cd681ca1f51b14615c718336
John J Schultz <rambotech@users.noreply.github.com>
2026-01-01 12:03:18 -0500

    DateSequence WIP

--- BOG.ClipboardMunger.Common/MethodLibrary/DateSequencePattern.cs
+++ BOG.ClipboardMunger.Common/MethodLibrary/DateSequencePattern.cs
@@ -11,70 +11,83 @@ namespace BOG.ClipboardMunger.Common.MethodLibrary
     {
         public override string MethodName { get => "Date Sequence Pattern"; }
         public override string GroupName { get => "Time"; }
-        public override string Description { get => "Make a list of dates per rules"; }
+        public override string Description { get => "Make a list of dates per pattern (see examples)"; }
 
         private StringBuilder Result = new StringBuilder();
 
         public DateSequencePattern()
         {
-            //base.SetArgument(new Argument
-            //{
-            //    Name = "StartDate",
-            //    Title = "Starting Date",
-            //    DefaultValue = DateTime.Now.ToString("MM/dd/yyyy"),
-            //    Description = "",
-            //    ValidatorRegex = @"[\d]{2}-[\d]{2}-[\d]{4}"
-            //});
+            // Pattern is...
+            //  StartDate;Step;Milestones;Rectify;EndDate;date output format
+            //      Step is #D = # of day, #M = # of month, #W = # of weeks, #D = # of days
+            //      Milestones: comma delimited list of day of week values, or day of month values.  Empty for everything
+            //          Day or Week milestone: 0 = Sun, ..., 6 = Sat.  0,6 = Sunday and Saturday.
+            //          Month milestone: 
+
+            base.SetArgument(new Argument
+            {
+                Name = "Pattern",
+                Title = "Pattern of Date repitiion",
+                DefaultValue = "MM/DD/YYYY;M;15,EOM;SAT,SUN;MM/DD/YYYY;yyyy-MM-dd",
+                Description = "The first date to generate",
+                ValidatorRegex = @"[\d]{2}/[\d]{2}/[\d]{4}"
+            });
+
+            base.SetArgument(new Argument
+            {
+                Name = "Pattern",
+                Title = "Pattern of Date repitiion",
+                DefaultValue = "MM/DD/YYYY;M;15,EOM;SAT,SUN;MM/DD/YYYY;yyyy-MM-dd",
+                Description = "The first date to generate",
+                ValidatorRegex = @"[\d]{2}/[\d]{2}/[\d]{4}"
+            });
 
             #region Examples
-#if FALSE
-            base.Examples.Add("Weekly 2025", new Example
+            base.Examples.Add("Weekly, all days, March 1, 2025 to December 31, 2025", new Example
             {
-                Name = "Weekly 2025",
+                Name = "Weekly All Days, 2025",
                 ArgumentValues = new Dictionary<string, string>
                 {
-                    {"Year", "2025" },
-                    {"PayFrequency", "W" },
-                    {"OutputFormat", "MM/dd/yyyy" }
+                    {"Pattern", "3/1/2025;W;*;12/31/2025" }
                 }
             });
-            base.Examples.Add("BiWeekly 2025", new Example
+            base.Examples.Add("Weekly, some days, March 1, 2025 to December 31, 2025", new Example
             {
-                Name = "BiWeekly 2025",
+                Name = "Weekly Some Days (Tue Thu Sat), 2025",
                 ArgumentValues = new Dictionary<string, string>
                 {
-                    {"Year", "2025" },
-                    {"PayFrequency", "B" },
-                    {"OutputFormat", "MM/dd/yyyy" }
+                    {"Pattern", "3/1/2025;W;Tue,Thu,Sat;12/31/2025" }
                 }
             });
-            base.Examples.Add("SemiMonthly 2025", new Example
+            base.Examples.Add("Bi-Weekly, some days, March 1, 2025 to December 31, 2025", new Example
             {
-                Name = "SemiMonthly 2025",
+                Name = "Weekly Some Days (Fri), 2025",
                 ArgumentValues = new Dictionary<string, string>
                 {
-                    {"Year", "2025" },
-                    {"PayFrequency", "S" },
-                    {"OutputFormat", "MM/dd/yyyy" }
+                    {"Pattern", "3/1/2025;W;Fri;12/31/2025" }
                 }
             });
-            base.Examples.Add("Monthly 2025", new Example
+            base.Examples.Add("Monthly, specific days, March 1, 2025 to December 31, 2025", new Example
             {
-                Name = "Monthly 2025",
+                Name = "Monthly Specific Days (15,EOM), 2025",
                 ArgumentValues = new Dictionary<string, string>
                 {
-                    {"Year", "2025" },
-                    {"PayFrequency", "M" },
-                    {"OutputFormat", "MM/dd/yyyy" }
+                    {"Pattern", "3/1/2025;M;15,EOM;12/31/2025" }
                 }
             });
-#endif
-#endregion
+            #endregion
         }
 
         public override string Munge(string textToMunge)
         {
-            var startDate = DateTime.Parse("01/01/2025");
+            var pattern = base.GetArgumentValue("Pattern");
+
+            var arguments = pattern.Split(new char[] { ';'}, StringSplitOptions.None);
+            if (arguments.Length != 6)
+            {
+                throw new Exception($"Pattern requires 6 arguments, but has {arguments.Length}");
+            }
+            var startDate = DateTime.Parse(arguments[0]);
             var endDate = DateTime.Parse("12/01/2025");
 
             while (startDate <= endDate)
